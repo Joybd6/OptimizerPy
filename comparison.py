2cb62c8e5b6cbce53a9d5729758691a0f1d4111c
Ajoy Mahanta <25824631+Joybd6@users.noreply.github.com>
2023-08-24 22:59:07 +0600

    Getting fitness data

---
+++ comparison.py
@@ -0,0 +1,37 @@
+from hhgso import load_HOP
+from tcheckop import load_TET
+from benchmarking.benchmark import Rastrigin, Ackley, Step, Schwefel_1_2, Rosenbrock, Quartic_with_noise, Sphere
+import matplotlib.pyplot as plt
+import os
+import pandas as pd
+
+ackley = Rastrigin()
+
+#plt.title(f"Benchmark ({ackley.name}\)")
+eop = load_TET(ackley)
+eop.run(100)
+#eop.history.get("fitness")
+
+#print(os.getcwd())
+
+df = pd.DataFrame(eop.history.get("fitness"),columns=["fitness"])
+df.to_csv(f"results/{ackley.name}_Triplecheck.csv", index=False)
+
+
+#hop = load_HOP(ackley)
+#hop.run(100)
+#print(hop.history)
+#print(eop.history)
+#eop.history.name = "EHGO"
+#hop.history.name = "HHGSO"
+""" def plot_history(history,save_path):
+    plt.title(f"Benchmark ({ackley.name})")
+    plt.xlabel("Iteration")
+    plt.ylabel("Optimal Value")
+    for i in history:
+        i.plot()
+    plt.legend()
+    save_path = os.path.join(save_path, f"{ackley.name}.svg")
+    plt.savefig(save_path, format="svg")
+
+plot_history([eop.history, hop.history], "../images/") """
---
+++ hhgso.py
@@ -0,0 +1,90 @@
+import numpy as np
+from copy import deepcopy
+from optimizers.population import Population
+# Import HGSO
+from optimizers.algorithms import HGSO
+from optimizers.algorithms import hgso_callable
+# Import SinCos
+from optimizers.algorithms import SineCos
+from optimizers.algorithms import sinecos_callable
+# Import Jaya
+from optimizers.algorithms import Jaya
+from optimizers.algorithms import jaya_callable
+# Import Butterfly
+from optimizers.algorithms import Butterfly
+from optimizers.algorithms import butterfly_callable
+# Import OwlSearch
+from optimizers.algorithms import OwlSearch
+from optimizers.algorithms import owl_search_callable
+from benchmarking.benchmark import *
+# Import Optimizers
+from optimizers import Optimizer
+
+# Import Custom Optimizer
+from optimizers.custom import HOptimizer
+import matplotlib.pyplot as plt
+
+
+#### Checking the algorithms
+
+obj = Schwefel_1_2()
+
+
+def load_HOP(obj):
+    dimension = 30
+    pop_size = 100
+    upper_bound = np.array([obj.range[1]]).repeat(dimension)
+    lower_bound = np.array([obj.range[0]]).repeat(dimension)
+    algorithms = []
+    calls = []
+
+    # Sphere function
+
+
+
+    # Population initialization
+    population = Population(lower_bound, upper_bound, size=pop_size, dimension=dimension, objective_function=obj.get_algorithm(),
+                            optimization="min")
+    population.initialize(initializer="uniform", lower_bound=lower_bound, upper_bound=upper_bound)
+
+    # Algorithms initialization
+    # HGSO
+    hgso_params = {'n_cluster': 5, "cluster_size": 20, "alpha": 1.0, "beta": 1.0}
+    hgso = HGSO(dimension=dimension, **hgso_params)
+    algorithms.append(hgso)
+    calls.append(hgso_callable)
+
+    # SineCos
+    sinecos_params = {'a': 0.8}
+    sinecos = SineCos(dimension=dimension, **sinecos_params)
+    algorithms.append(sinecos)
+    calls.append(sinecos_callable)
+
+    # Jaya
+    jaya_params = {}
+    jaya = Jaya(dimension=dimension, **jaya_params)
+    algorithms.append(jaya)
+    calls.append(jaya_callable)
+
+    # Butterfly
+    butterfly_params = {'c': 0.2, 'p': 0.8}
+    butterfly = Butterfly(dimension=dimension, **butterfly_params)
+    algorithms.append(butterfly)
+    calls.append(butterfly_callable)
+
+    # Owl Search
+    owl_search_params = {}
+    owl_search = OwlSearch(dimension=dimension, **owl_search_params)
+    algorithms.append(owl_search)
+    calls.append(owl_search_callable)
+
+    # HOptimizer
+    hoptimizer = HOptimizer(obj.get_algorithm(), population, algorithms, calls)
+
+    return hoptimizer
+    #hoptimizer.run(100)
+    #hoptimizer.history.name = f"Hybrid Henry Gas Solubility Optimization ({obj.name})"
+    #hoptimizer.history.plot()
+    #plt.show()
+#print(hoptimizer.population.global_optimum)
+#print("-----------------------------------")
